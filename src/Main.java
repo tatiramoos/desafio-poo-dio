8ebc9e8c505e1aa24641b86879dc6e97c90f8569
Tatiane Ramos <tatitsramos@gmail.com>
2022-07-19 16:45:11 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,8 +23,37 @@ public class Main {
         mentoria.setDescricao("descricao mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devTatiane = new Dev();
+        devTatiane.setNome("Tatiane");
+        devTatiane.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Tatiane" + devTatiane.getConteudosInscritos());
+        devTatiane.progredir();
+        System.out.println("Conteúdos Inscritos Tatiane" + devTatiane.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Tatiane" + devTatiane.getConteudosConcluidos());
+        System.out.println("XP: " + devTatiane.calcularXp());
+
+        System.out.println("---------");
+
+        Dev devDiego = new Dev();
+        devDiego.setNome("Diego");
+        devDiego.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos Diego" + devDiego.getConteudosInscritos());
+        devDiego.progredir();
+        System.out.println("Conteúdos Inscritos Diego" + devDiego.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos Diego" + devDiego.getConteudosConcluidos());
+        System.out.println("XP: " + devDiego.calcularXp());
+
+        System.out.println("---------");
     }
 }
